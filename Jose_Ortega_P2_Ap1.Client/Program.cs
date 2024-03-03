58cc3e3842d3bd41fe1d7f9cf5606e4d78ee5c62
Stiinger <ortegantony29@gmail.com>
2024-03-03 14:39:42 -0400

    Add project files.

---
+++ Jose_Ortega_P2_Ap1.Client/Program.cs
@@ -0,0 +1,11 @@
+using Jose_Ortega_P2_Ap1.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5004") });
+
+await builder.Build().RunAsync();
