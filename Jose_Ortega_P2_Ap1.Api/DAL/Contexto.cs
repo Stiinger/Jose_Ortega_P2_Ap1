6a8b7bb5d0156b376a427a3012c60af9e5cfdbdb
SirLevid <jassontejada@gmail.com>
2024-03-04 20:27:33 -0400

    Modificar contexto.

--- Jose_Ortega_P2_Ap1.Api/DAL/Contexto.cs
+++ Jose_Ortega_P2_Ap1.Api/DAL/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
 namespace Jose_Ortega_P2_Ap1.Api.DAL;
 
@@ -6,6 +7,21 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options)
     {
-        
+    }
+
+    public DbSet<Vehiculos> Vehiculos { get; set; }
+    public DbSet<Accesorios> Accesorios { get; set; }
+    public DbSet<VehiculosDetalle> VehiculosDetalle { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Accesorios>().HasData(
+            new Accesorios() { AccesorioId = 1, Descripcion = "Camara trasera" },
+            new Accesorios() { AccesorioId = 2, Descripcion = "Pantalla interior" },
+            new Accesorios() { AccesorioId = 3, Descripcion = "Interior en piel" },
+            new Accesorios() { AccesorioId = 4, Descripcion = "Sun Roof" },
+            new Accesorios() { AccesorioId = 5, Descripcion = "Aros de lujo" }
+        );
+        base.OnModelCreating(modelBuilder);
     }
 }
