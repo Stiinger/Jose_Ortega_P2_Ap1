5d96907f57c205f01943b701e9ddcf2219f0dbdb
SirLevid <jassontejada@gmail.com>
2024-03-04 20:27:13 -0400

    Crear modelo de accesorios

---
+++ Shared/Models/Accesorios.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Models;
+
+public class Accesorios
+{
+    [Key]
+    public int AccesorioId { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [ForeignKey("Accesorios")]
+    public ICollection<VehiculosDetalle> VehiculosDetalle { get; set; } = new List<VehiculosDetalle>();
+}
