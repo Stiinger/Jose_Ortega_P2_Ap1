1bd6bb1cd985c45ef1671f40bce3979e78559af4
SirLevid <jassontejada@gmail.com>
2024-03-04 20:27:23 -0400

    Crear Detalle de Vehiculos

---
+++ Shared/Models/VehiculosDetalle.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models;
+
+public class VehiculosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int VehiculoId { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public int AccesorioId { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public decimal Valor {  get; set; }
+}
